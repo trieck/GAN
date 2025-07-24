9c0e700f2db4b57eaf3531afe48ef8dab75e95f3
Thomas A. Rieck <tom.rieck@maxar.com>
2025-07-24 13:00:16 -0400

    Use spectral normalization for the discriminator.

--- models/discriminator.py
+++ models/discriminator.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+from torch.nn.utils import spectral_norm
 
 
 class Discriminator(nn.Module):
@@ -7,27 +8,27 @@ class Discriminator(nn.Module):
         super().__init__()
         self.embedding = nn.Embedding(10, 28 * 28)
         self.net = nn.Sequential(
-            nn.Conv2d(2, 64, kernel_size=4, stride=2, padding=1),  # 28 → 14
+            spectral_norm(nn.Conv2d(2, 64, kernel_size=4, stride=2, padding=1)),  # 28 → 14
             nn.BatchNorm2d(64),
             nn.LeakyReLU(0.2, inplace=True),
 
-            nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1),  # 14 → 7
+            spectral_norm(nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1)),  # 14 → 7
             nn.BatchNorm2d(128),
             nn.LeakyReLU(0.2, inplace=True),
             nn.Dropout(0.3),
 
-            nn.Conv2d(128, 256, kernel_size=4, stride=2, padding=1),  # 7 → 4
+            spectral_norm(nn.Conv2d(128, 256, kernel_size=4, stride=2, padding=1)),  # 7 → 4
             nn.BatchNorm2d(256),
             nn.LeakyReLU(0.2, inplace=True),
             nn.Dropout(0.3),
 
-            nn.Conv2d(256, 512, kernel_size=4, stride=2, padding=1),  # 4 → 2
+            spectral_norm(nn.Conv2d(256, 512, kernel_size=4, stride=2, padding=1)),  # 4 → 2
             nn.BatchNorm2d(512),
             nn.LeakyReLU(0.2, inplace=True),
 
-            nn.AdaptiveAvgPool2d(1),  # forces spatial output to 1×1
+            nn.AdaptiveAvgPool2d(1),
             nn.Flatten(),
-            nn.Linear(512, 1),
+            spectral_norm(nn.Linear(512, 1)),
         )
 
     def forward(self, imgs, labels):
